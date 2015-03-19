4ea3dea6b8fab64e28cc413769fb7b1d1b72ec86
Andres <afmontanar@gmail.com>
2015-03-19 10:29:25 -0500

    Interfaz grafica cliente realizada, hay que trabajar en el guardar y validar

--- misitio/clases/static/js/datagrid.js
+++ misitio/clases/static/js/datagrid.js
@@ -1,44 +1,76 @@
-require(['dojo/_base/lang', 'dojox/grid/DataGrid', 'dojo/data/ItemFileWriteStore', 'dojox/grid/cells/dijit', 'dojo/dom', 'dojo/domReady!'],
-  function(lang, DataGrid, ItemFileWriteStore, cells, dom){
+require(['dojo/_base/lang', 'dojox/grid/DataGrid', 'dojo/data/ItemFileWriteStore', 'dijit/form/Button', 'dojo/dom', 'dojo/domReady!'],
+
+function (lang, DataGrid, ItemFileWriteStore, Button, dom) {
     /*set up data store*/
     var data = {
-      identifier: "id",
-      items: []
+        items: []
     };
-    var data_list = [
-    { col1: "normal", col2: false, col3: 'But are not followed by two hexadecimal', col4: "store", col5: "la",col6: "aa"}
-    /*{ col1: "normal", col2: false, col3: 'But are not followed by two hexadecimal', col4: 29.91},
-      { col1: "important", col2: false, col3: 'Because a % sign always indicates', col4: 9.33},
-      { col1: "important", col2: false, col3: 'Signs can be selectively', col4: 19.34}
-    */];
-   /* var rows = 60;
-    for(var i = 0, l = data_list.length; i < rows; i++){
-      data.items.push(lang.mixin({ id: i+1 }, data_list[i%l]));
-    }*/
-    var store = new ItemFileWriteStore({data: data});
-   
+
+    var store = new ItemFileWriteStore({
+        data: data
+    });
+
     /*set up layout*/
-    var layout = [[
-      {'name': 'Identificacion', 'field': 'id', 'width': '100px', editable: true},
-      {'name': 'Nombres', 'field': 'col2', 'width': '100px', editable: true, type: dojox.grid.cells.CheckBox,styles: 'text-align: center;'},
-      {'name': 'Apellidos', 'field': 'col3', 'width': '200px', editable: true},
-      {'name': 'Direccion', 'field': 'col4', 'width': '150px', editable: true},
-      {'name': 'Celular', 'field': 'id', 'width': '100px', editable: true},
-      {'name': 'Detalle', 'field': 'col3', 'width': '200px', editable: true}
-    ]];
+    var layout = [
+        [{
+            'name': 'Identificacion',
+                'field': 'col1',
+                'width': '100px',
+                 editable: true
+        }, {
+            'name': 'Nombres',
+                'field': 'col2',
+                'width': '100px',
+                 editable: true
+        }, {
+            'name': 'Apellidos',
+                'field': 'col3',
+                'width': '200px',
+                 editable: true
+        }, {
+            'name': 'Direccion',
+                'field': 'col4',
+                'width': '150px',
+                 editable: true
+        }, {
+            'name': 'Celular',
+                'field': 'col5',
+                'width': '150px',
+                 editable: true
+        }, {
+            'name': 'Detalle',
+                'field': 'col6',
+                'width': '150px',
+                 editable: true
+        }]
+    ];
 
     /*create a new grid*/
     var grid = new DataGrid({
         id: 'grid',
         store: store,
         structure: layout,
-        rowSelector: '20px'});
+        rowSelector: '20px'
+    });
 
     /*append the new grid to the div*/
     grid.placeAt("gridDiv");
 
     /*Call startup() to render the grid*/
     grid.startup();
-    grid.addRow({ col1: "normal", col2: false, col3: 'But are not followed by two hexadecimal', col4: "store", col5: "la",col6: "aa"});
 
+    var button = new Button({
+        onClick: function () {
+            console.log(arguments);
+            store.newItem({
+                col1: "col1-",
+                col2: "col2-",
+                col3: "col3-",
+                col4: "col4-",
+                col5: "col5-",
+                col6: "col6-"
+            });
+        
+        }
+    }, "addRow");
 });
